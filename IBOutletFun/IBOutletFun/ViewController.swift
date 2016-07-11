ed7625197c5980e5be25410c46205cff423df188
sheafk <sheafureyking@gmail.com>
2016-07-11 10:00:41 -0400

    Finished lab. Learned about IBOutlets and IBActions in Swift.

--- IBOutletFun/IBOutletFun/ViewController.swift
+++ IBOutletFun/IBOutletFun/ViewController.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textDisplay: UILabel!
+    
+    @IBAction func helloWorld(sender: AnyObject) {
+        print("Pressed Hello World button")
+        textDisplay.text = "Hello, world!"
+
+    }
+    @IBAction func goodbyeWorld(sender: AnyObject) {
+        
+        print("Pressed Good Bye World button")
+        textDisplay.text = "Goodbye, World!"
+    }
+    @IBAction func unicorn(sender: AnyObject) {
+        print("Pressed Unicorn! button")
+        textDisplay.text = "🦄"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzz"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
